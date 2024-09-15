68062aff3f8ef4a8c9bce4497c272f0cd0f691c7
Divyapunitharaju <ptdivya04@gmail.com>
2024-09-15 10:33:52 +0530

    commit

---
+++ quiz-app/src/assets/data.js
@@ -0,0 +1,25 @@
+const data =[{
+    question:"Which device is required for the internet connnection",
+    option1:"Moderm",
+    option2:"Router",
+    option3:"LAN Cable",
+    options4:"Pen Drive",
+    ans:1
+},{
+    question:"Which continent has the highest number of countries",
+    option1:"Asia",
+    option2:"Europe",
+    option3:"North America",
+    options4:"Africa",
+    ans:4
+},{
+    question:"what is the national bird of India?",
+    option1:"Crow",
+    option2:"Peakcock",
+    option3:"Pegion",
+    options4:"Parrot",
+    ans:3
+}
+
+
+]
